1ad0083819fa85b5a1bd28c93917cf26d2d31467
Viktor <lightoton@gmail.com>
2023-09-27 09:31:30 +0200

    Homework1.3

--- src/main/java/de/telran/homework1/Task2test.java
+++ src/main/java/de/telran/homework1/Task2test.java
@@ -12,11 +12,11 @@ public class Task2test {
         } while (integer<100||integer>999);
         System.out.println("Вы ввели числo: "+ integer);
         int hundreds = (integer/ 100);
-        int dozens = (integer%100/ 10);
+        int Tens = (integer%100/ 10);
         int units = (integer% 10);
 
         System.out.println("Сотни: "+hundreds);
-        System.out.println("Десятки: "+dozens);
+        System.out.println("Десятки: "+Tens);
         System.out.println("Единицы: "+units);
         scanner.close();
 
