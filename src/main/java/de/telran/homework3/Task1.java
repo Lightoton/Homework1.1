35002e75d076509afb357094cf5b48712c3a017b
Viktor <lightoton@gmail.com>
2023-10-03 20:42:55 +0200

    Homework2.0

---
+++ src/main/java/de/telran/homework3/Task1.java
@@ -0,0 +1,38 @@
+package de.telran.homework3;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Напишите первое слово с чётным количеством символов: ");
+        String myStr1 = scanner.next();
+        int myLength1 = myStr1.length();
+        if (myLength1%2 ==0  ){
+            System.out.println("Вы написали: "+myStr1);
+        } else {
+            System.out.println("Вы написали слово с нечётным количеством букв!!! Ошибка!!!");
+            return;
+        }
+
+        System.out.print("Введите  второе слово с чётным количеством символов: ");
+        String myStr2 = scanner.next();
+        int myLength2 = myStr2.length();
+        if (myLength2 %2 ==0){
+            System.out.println("Вы написали: "+myStr2);
+        } else {
+            System.out.println("Вы написали слово с нечётным количеством букв!!! Ошибка!!!");
+            return;
+        }
+        int myHalfStr1 = myLength1/2;
+        int myHalfStr2 = myLength2/2;
+
+        System.out.println("Ваше новое слово состоящее из половинок первого и второго: "+myStr1.substring(0,myHalfStr1)+myStr2.substring(myHalfStr2));
+
+    }
+}
+//Введите 2 слова, воспользуйтесь сканером, состоящие из четного количества букв (проверьте количество букв в слове).
+//Нужно получить слово, состоящее из первой половины первого слова и второй половины второго слова. распечатать на консоль.
+//Например:
+//ввод - mama, papa
+//вывод - mapa
---
+++ src/main/java/de/telran/homework3/Task2.java
@@ -0,0 +1,48 @@
+package de.telran.homework3;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первый аргумент: ");
+        int arg1 = scanner.nextInt();
+        System.out.print("Введите второй аргумент: ");
+        int arg2 = scanner.nextInt();
+        printSum(arg1,arg2);
+        printDif(arg1,arg2);
+        printMult(arg1,arg2);
+        printDiv(arg1,arg2);
+
+    }
+
+    static void printSum(int a, int b){
+        int sum = a+b;
+        System.out.println("Сумма= "+sum);
+    }
+    static void printDif(int a, int b){
+        int dif = a-b;
+        System.out.println("Разность= "+dif);
+
+    }
+    static void printMult(int a, int b){
+        int mult = a*b;
+        System.out.println("Произведение= "+mult);
+    }
+    static void printDiv(int a, int b) {
+        if (b!=0) {
+            double div = (double) a/b;
+
+            System.out.println("Частное= "+div);
+        } else
+            System.out.println("Частное= Ошибка!!! Делить на ноль нельзя!!!");
+
+    }
+}
+//Реализовать программу, выводящую на экран результаты:
+//Сложения двух чисел
+//Вычитания двух чисел
+//Умножения двух чисел
+//Деления двух чисел
+//Каждая из арифметических операций должна быть реализована как отдельный метод.
+//***Сделал другим методом в отличии от прошлого ДЗ(homework/Task2)
